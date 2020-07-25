e52a2a777b6d23be52bad30e05c805de4ebca6fa
Balzhinimaev Lopson <balzhinimaevlb@m2.ru>
2020-07-25 21:14:30 +0300

    Upgrade ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,46 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'import', 'react-hooks'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    root: true,
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+        es6: true,
+        jest: true,
+    },
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal'],
+                'newlines-between': 'always-and-inside-groups',
+            },
+        ],
+        '@typescript-eslint/camelcase': 0,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };

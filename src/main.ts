a7484512d436f6c550fedac0450c7e5092f14b84
Balzhinimaev Lopson <balzhinimaevlb@m2.ru>
2020-07-28 09:18:00 +0300

    Added configuration for AWS service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import * as config from 'config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const serverConfig = config.get('server');
+
+    const logger = new Logger('bootstrap');
+    const app = await NestFactory.create(AppModule);
+
+    const port = process.env.PORT || serverConfig.port;
+    await app.listen(port);
+    logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,16 +1,22 @@
 import { Repository, EntityRepository } from 'typeorm';
+import { Logger, InternalServerErrorException } from '@nestjs/common';
 
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { User } from '../auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    private logger = new Logger('TaskRepository');
+
+    async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
         const { status, search } = filterDto;
         // Queru builder is helpful to create custom queries to DB
         const query = this.createQueryBuilder('task');
+
+        query.where('task.userId = :userId', { userId: user.id });
 
         if (status) {
             // "andWhere" will not override others conditions, but "where" will.
@@ -24,18 +30,42 @@ export class TaskRepository extends Repository<Task> {
             });
         }
 
-        const tasks = await query.getMany();
-        return tasks;
+        try {
+            const tasks = await query.getMany();
+            return tasks;
+        } catch (error) {
+            this.logger.error(
+                `Failed to get all tasks for user ${user.username}, Filters: ${JSON.stringify(
+                    filterDto,
+                )}`,
+                error.stack,
+            );
+            throw new InternalServerErrorException();
+        }
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
         const { title, description } = createTaskDto;
         const task = new Task();
         task.title = title;
         task.description = description;
         task.status = TaskStatus.OPEN;
-        await task.save();
+        task.user = user;
+        try {
+            await task.save();
+        } catch (error) {
+            this.logger.error(
+                `Failed to create a task for user ${user.username}. Data: ${JSON.stringify(
+                    createTaskDto,
+                )}`,
+                error.stack,
+            );
+            throw new InternalServerErrorException();
+        }
 
+        // Before returning user deleting user information:
+        delete task.user;
         return task;
     }
 }
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iktyb2tvMSIsImlhdCI6MTU5NTkxNDAzNCwiZXhwIjoxNTk1OTE3NjM0fQ.fZJG2gMYuGJmnlJUAq8IflOkwLz6K3FcFr1UPW8jIDA

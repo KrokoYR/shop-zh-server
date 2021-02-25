d09dbf1af511220f22b5c4489c60378214a35818
Lopson <balzhinimaev-2011@mail.ru>
2021-02-25 18:55:44 +0300

    Heroku release

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+// Modules:
 import { TasksModule } from './tasks/tasks.module';
-import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+
+// Configs:
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
     imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule, ProductsModule],
